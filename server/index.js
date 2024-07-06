b5fd04c56f9e92850ef52fa4befec7b39ca4d2ed
KhushiSharma2448 <105037509+KhushiSharma2448@users.noreply.github.com>
2024-07-06 15:13:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,12 +8,7 @@ const authRoutes=require("./routes/auth.js")
 const listingRoutes=require("./routes/listing.js")
 const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
-app.use(cors(
-  { origin:[""],
-  methods:["POST","GET"],
-  credentials:true
-  }
-));
+app.use(cors);
 app.use(express.json());
 app.use(express.static('public'));
 
